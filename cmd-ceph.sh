7e4a9e372c3d16ffedcf6e1f2fee23a5d40e9bec
kaosay <547091630@qq.com>
2025-06-06 15:06:34 +0800

    ceph

--- cmd-ceph.sh
+++ cmd-ceph.sh
@@ -8,5 +8,19 @@ rbd children jing.meta/csi-vol-755
 
 # command usually
 
+# show status of ceph cluster
 ceph --status
 ceph -s
+
+# show pools of ceph
+ceph osd pool ls
+
+# Show cluster’s free space status.
+ceph df {detail}
+
+# Subcommand ls to list file systems
+ceph fs ls
+
+# Show cluster’s health.
+ceph health {detail}
+

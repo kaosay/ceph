77ee30c7c2863a99155a37f3f8c72f38dade177f
kaosay <547091630@qq.com>
2025-06-24 16:26:09 +0800

    rbd

---
+++ shell/jingsuai.rbd.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# create rbd erasure on ceph
+
+
+sudo ceph osd pool create jingsuai-rbd erasure
+
+sudo ceph osd pool get jingsuai-rbd all
+
+sudo ceph osd pool set jingsuai-rbd allow_ec_overwrites true
+
+sudo ceph osd pool  set jingsuai-rbd fast_read 1
+
+sudo ceph osd pool create jingsuai-rbd.meta 128
+
+
+sudo ceph osd pool application enable jingsuai-rbd rbd
+sudo ceph osd pool application enable jingsuai-rbd.meta rbd
+
+ceph auth caps client.jingsuai \
+  mon 'allow r' \
+  mds 'allow rw, allow rws path=/' \
+  osd 'allow rwx pool=jingsuai-data, allow rwx pool=jingsuai-data-ec, allow rwx pool=jingsuai-metadata, allow rwx pool=jingsuai-rbd, allow rwx pool=jingsuai-rbd.meta'
+
+#sudo ceph auth get-or-create client.jingsuai mon 'allow r' osd 'allow rwx pool=jingsuai-rbd, allow rwx pool=jingsuai-rbd.meta' -o /etc/ceph/ceph.client.jingsuai.keyring
+
+sudo ceph auth get client.jingsuai
+

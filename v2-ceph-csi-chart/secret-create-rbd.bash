1f553efecae9a6d83b327de7ab745512c0c09ac2
kaosay <kaosay@qq.com>
2025-10-18 20:05:00 +0800

    v2

---
+++ v2-ceph-csi-chart/secret-create-rbd.bash
@@ -0,0 +1,2 @@
+
+kubectl create secret generic csi-rbd-secret --namespace=ceph-rbd --from-literal=userID=jsy-test --from-literal=userKey=AQBBMvNoMNCDOxAA4YciHEogAue3YIKc1gqVNg==
---
+++ v2-ceph-csi-chart/secret-create.bash
@@ -0,0 +1,2 @@
+
+kubectl create secret generic csi-cephfs-secret --namespace=cephfs --from-literal=adminID=admin --from-literal=adminKey=AQAFLpxo3e5fKBAAjNEOQjfTXj3kdnZ7GJvxzg==
